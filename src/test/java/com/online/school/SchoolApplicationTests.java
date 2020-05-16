4c1e9b25db2cbdbe378604961750d050fe2c6729
RajneeshYadav <36070833+RajneeshEr@users.noreply.github.com>
2020-05-16 12:34:57 +0530

    initial commit

---
+++ src/test/java/com/online/school/SchoolApplicationTests.java
@@ -0,0 +1,14 @@
+package com.online.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
